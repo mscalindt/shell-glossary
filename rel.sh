e984f207603a9bc90fa1896878761209a116a4b6
Dimitar Yurukov <mscalindt@protonmail.com>
2025-09-13 04:14:19 +0200

    rel.sh: Recognize `src: ` as core change tag
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- rel.sh
+++ rel.sh
@@ -24,7 +24,7 @@ SRC_LOG=$(
                 *'(): [doc]'*)
                     continue
                 ;;
-                *'(): '*)
+                *'src: '* | *'(): '*)
                     printf "%s\n" "$LINE"
                 ;;
                 *)
